ec465baf384745f1fb77de26c723737bafd63b53
kloudCoder <dev@ezyy.cloud>
2023-02-20 13:29:09 +0200

    Updated footer

--- src/layouts/landing/Footer.vue
+++ src/layouts/landing/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <v-footer class="footer">
     <v-col class="text-center" cols="12">
-      {{ new Date().getFullYear() }} — <strong>Ezyy&#174;</strong>
+      <strong>Ezyy Inc&#174; </strong> — A division of the King Shayne Group — {{ new Date().getFullYear() }}
     </v-col>
   </v-footer>
 </template>
